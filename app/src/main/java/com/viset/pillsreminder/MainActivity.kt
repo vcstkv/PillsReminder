ad8d792d68f51046ce0e0a158fdf6356916c6d41
Viacheslav Tekaev <tekaeff@yandex.ru>
2021-02-22 23:44:50 +0300

    Add simple toolbar

--- app/src/main/java/com/viset/pillsreminder/MainActivity.kt
+++ app/src/main/java/com/viset/pillsreminder/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.viset.pillsreminder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.Boolean as Boolean
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> {
+                Toast.makeText(this, "Add item", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.settings_item -> {
+                Toast.makeText(this, "Settings item", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            else -> {
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                Toast.makeText(this, "unk item", Toast.LENGTH_SHORT).show()
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
 }
